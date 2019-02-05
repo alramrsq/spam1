884a66b95503b48936e457aa08bbefc90809c6e9
alramrsq <47371225+alramrsq@users.noreply.github.com>
2019-02-06 00:00:50 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("542479328521355283")
+setInterval(function() {
+channel.send(`sdfg`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
